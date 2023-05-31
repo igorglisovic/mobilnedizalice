d6f32594a0373575e64136bff479448f3ed667bd
Gliso <igorglisovic2@gmail.com>
2023-06-01 00:30:40 +0200

    added video2 section

--- src/components/Sections/VideoSection2.jsx
+++ src/components/Sections/VideoSection2.jsx
@@ -1,11 +1,24 @@
 import classes from './VideoSection2.module.css'
 import Container from '../UI/Container'
+import video2 from '../../assets/video2.mp4'
+import arrow from '../../assets/arrow.png'
+import thumbnail from '../../assets/thumbnail2.jpg'
 
 const VideoSection2 = () => {
   return (
-    <section className={classes.video}>
+    <section className={classes['video-section']}>
       <Container>
-        <div>{/* <video src=""></video> */}</div>
+        <div className={classes['video-wrapper']}>
+          <video
+            className={classes.video}
+            controls
+            src={video2}
+            poster={thumbnail}
+          >
+            <p>Your browser does not recognize video element.</p>
+          </video>
+          <div></div>
+        </div>
       </Container>
     </section>
   )
