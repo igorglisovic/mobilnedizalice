2d15727808a7f2e73c035a0c08a7c39579dd0a28
Gliso <igorglisovic2@gmail.com>
2023-06-02 19:16:34 +0200

    final touches

--- src/components/UI/ContactForm.jsx
+++ src/components/UI/ContactForm.jsx
@@ -1,8 +1,37 @@
+import { useRef } from 'react'
 import classes from './ContactForm.module.css'
+import emailjs from '@emailjs/browser'
 
 const ContactForm = ({ style }) => {
+  const form = useRef()
+
+  const sendEmail = e => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'YOUR_SERVICE_ID',
+        'YOUR_TEMPLATE_ID',
+        form.current,
+        'YOUR_PUBLIC_KEY'
+      )
+      .then(
+        result => {
+          console.log(result.text)
+        },
+        error => {
+          console.log(error.text)
+        }
+      )
+  }
+
   return (
-    <form style={style} className={classes.form}>
+    <form
+      ref={form}
+      style={style}
+      onSubmit={sendEmail}
+      className={classes.form}
+    >
       <h2>Kontakt forma</h2>
       <div>
         <label htmlFor="name">Vaše ime</label>
